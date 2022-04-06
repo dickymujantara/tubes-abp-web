8d624b9c1170d9974a165bcbec26d10d373fbd0a
dickymujantara <dickymujantara@gmail.com>
2022-04-06 10:39:36 +0700

    add column user

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,12 +15,13 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('username');
+            $table->string('username')->unique();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('role');
-            $table->boolean('is_active')->default(1);
+            $table->string('email_verification_token');
+            $table->string('has_verified_email')->integer(4);
+            $table->string('address');
             $table->rememberToken();
             $table->timestamps();
         });
